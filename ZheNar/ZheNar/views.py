72f88b7eae4ebce2c3e1ca8ae7be6cc73eafa69e
francushu <huhexiang88@gmail.com>
2013-05-30 18:06:55 +0800

    set the under construction page to the root

---
+++ ZheNar/ZheNar/views.py
@@ -0,0 +1,9 @@
+from django.template import Template, Context
+from django.http import HttpResponse
+import datetime
+
+def under_construction(request):
+    now = datetime.datetime.now()
+    t = Template("<html><body><h1>The website is now under construction!!</h1>It is now {{ current_date }}.</body></html>")
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
