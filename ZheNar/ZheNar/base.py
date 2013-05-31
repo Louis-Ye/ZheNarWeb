3be7be3a1d4206a790d354307051bd9f65345262
LouisYe <yeluyuptd@163.com>
2013-05-31 21:22:25 +0800

    add the staticfiles and relative apps

--- ZheNar/ZheNar/base.py
+++ ZheNar/ZheNar/base.py
@@ -119,14 +119,16 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the  next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
     'south',
+
     'profiles',
     'events',
     'places',
