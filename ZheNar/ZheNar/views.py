58188a4217cca9624c4a346bdb4e45147a028e8f
francushu <huhexiang88@gmail.com>
2013-05-31 10:10:39 +0800

    create two new apps

--- ZheNar/ZheNar/views.py
+++ ZheNar/ZheNar/views.py
@@ -5,5 +5,6 @@ import datetime
 def under_construction(request):
     now = datetime.datetime.now()
     t = Template("<html><body><h1>The website is now under construction!!</h1>It is now {{ current_date }}.</body></html>")
-    html = t.render(Context({'current_date': now}))
+    html = t.render(Context({'current_date':now}))
     return HttpResponse(html)
+    
--- ZheNar/profiles/models.py
+++ ZheNar/profiles/models.py
@@ -3,3 +3,5 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile():
+	
