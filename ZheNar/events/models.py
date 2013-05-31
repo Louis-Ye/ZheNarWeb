3da9db899bf6527f906ea3cb6eeb982e5a852344
francushu <huhexiang88@gmail.com>
2013-05-31 14:28:22 +0800

    make the model of the apps

--- ZheNar/events/models.py
+++ ZheNar/events/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from profiles.models import Profile
+from places.models import Place
+ 
 # Create your models here.
+class Event(models.Model):
+	name = models.CharField(max_length = 255)
+	description = models.TextField(blank = True)
+	holder = models.ForeignKey(Profile)
+	host_organization = models.CharField(max_length = 255, null = True)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	place = models.ManyToManyField(Place)
+	event_type = models.ForeignKey(EventType,null = False)
+	
+	def __unicode__(self):
+		return self.name
+		
+	def event_lasting_time(self):
+		return endTime - startTime 
+	
+	
+	class Meta:
+		ordering = ['name']
+		
+class EventType(models.Model):
+	name = models.CharField(max_length = 255)
+	
+	def __unicode__(self):
+		return self.name
--- ZheNar/places/models.py
+++ ZheNar/places/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+	name = models.CharField(max_length = 255)
+	description = models.TextField(blank = True)
+	place_type = models.ForeignKey(EventType,null = False)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+
+	def __unicode__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['name']
+
+class PlaceType(models.Model):
+	name = models.CharField(max_length = 255)
+
+	def __unicode__(self):
+		return self.name
