c1e05681f4b0aac1e7bc2bb3378f099ec925a723
francushu <huhexiang88@gmail.com>
2013-06-12 13:44:09 +0800

    modify the profiles and authenticated

--- ZheNar/places/urls.py
+++ ZheNar/places/urls.py
@@ -4,4 +4,7 @@ from places import views
 
 urlpatterns = patterns('',
 	url(r'^$',views.index, name='index'),
+	#url(r'^create$', views.create,name='create'),
+	#url(r'^modify$',views.create,name='modify'),
+	
 )
--- ZheNar/places/views.py
+++ ZheNar/places/views.py
@@ -1,5 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from datetime import datetime
+from django.core.urlresolvers import reverse
+
+from profiles.models import Profile
+
 
 def index(request):
-	return HttpResponse("This is places index!")
+	if request.user.is_authenticated():
+		return HttpResponse("This is places index!")
+	else:
+		return HttpResponseRedirect(reverse('ZheNar.views.index'))
