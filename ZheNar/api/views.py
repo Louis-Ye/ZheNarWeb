55426ca114ad99c86c4b5515d266f0e5c2a76fec
LouisYe <yeluyuptd@163.com>
2013-06-13 14:32:07 +0800

    api post csrf_exempt

--- ZheNar/api/views.py
+++ ZheNar/api/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404, render
 from django.template import Context, loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.core.urlresolvers import reverse
@@ -35,17 +37,18 @@ def user(request):
 		return HttpResponse(json.dumps({"error_code": "NotLoggedIn", }), status=403)
 
 
+@csrf_exempt
+@require_POST
 def user_reg(request):
-	if request.POST:
-		username = request.POST.get('username')
-		email = request.POST.get('email')
-		password = request.POST.get('password')
-		studentid = request.POST.get('student_id')
-		name = request.POST.get('student_name')
-		gender = request.POST.get('gender')
-		registerTime = datetime.now()
-	else:
-		return HttpResponse(json.dumps({"error_code": "不是POST", }), status=400)
+	username = request.POST.get('username')
+	email = request.POST.get('email')
+	password = request.POST.get('password')
+	studentid = request.POST.get('student_id')
+	name = request.POST.get('student_name')
+	gender = request.POST.get('gender')
+	registerTime = datetime.now()
+	if not (username and email and password):
+		return HttpResponse(json.dumps({"error_code": "Can not be empty!", }), status=400)
 	
 	try:
 		upr = User.objects.get(username=username)	#check if there is duplicated username
@@ -73,12 +76,11 @@ def user_reg(request):
 	return HttpResponse(json.dumps({"id": upr.id, }))
 
 
+@csrf_exempt
+@require_POST
 def user_login_username(request):
-	if request.POST:
-		username = request.POST['username']
-		password = request.POST['password']
-	else:
-		return HttpResponse(json.dumps({"error_code": "不是POST", }), status=400)
+	username = request.POST['username']
+	password = request.POST['password']
 
 	upr = authenticate(username=username, password=password)
 	if upr is None:
@@ -90,12 +92,11 @@ def user_login_username(request):
 	return HttpResponse(json.dumps(user_info), mimetype='application/json')
 
 
+@csrf_exempt
+@require_POST
 def user_login_email(request):
-	if request.POST:
-		email = request.POST['email']
-		password = request.POST['password']
-	else:
-		return HttpResponse(json.dumps({"error_code": "不是POST", }), status=400)
+	email = request.POST['email']
+	password = request.POST['password']
 
 	upr = authenticate(email=email, password=password)
 	if upr is None:
@@ -107,9 +108,9 @@ def user_login_email(request):
 	return HttpResponse(json.dumps(user_info), mimetype='application/json')
 
 
+@csrf_exempt
+@require_POST
 def user_logout(request):
-	if not request.POST:
-		return HttpResponse(json.dumps({"error_code": "不是POST", }), status=400)
 	logout(request)
 	return HttpResponse("0")
 	
