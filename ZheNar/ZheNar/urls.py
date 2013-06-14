fcbe1da8c0ed8d4eef896b0982df5d1e0a241deb
francushu <huhexiang88@gmail.com>
2013-06-14 20:53:23 +0800

    设置admin页面

--- ZheNar/ZheNar/urls.py
+++ ZheNar/ZheNar/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', 'ZheNar.views.index'),
+	url(r'^$', 'ZheNar.views.index',name = 'index'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^api/', include('api.urls', namespace="api")),
 	url(r'^profiles/', include('profiles.urls', namespace="profiles")),
