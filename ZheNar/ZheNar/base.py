348faa44d00faa01504ce6aa7dcffcefc4c82f75
francushu <huhexiang88@gmail.com>
2013-06-14 19:52:02 +0800

    db设置文件

--- ZheNar/ZheNar/base.py
+++ ZheNar/ZheNar/base.py
@@ -19,7 +19,8 @@ DATABASES = {
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
         'OPTIONS': { 
-            'init_command':'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+            'init_command':'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci',
+            'read_default_file': './db/my.cnf',
         }
     }
 }
